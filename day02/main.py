ffb51d376c66336b4c7a78ce67a7d4b3732f4161
Lukas Deutz <github@deutz.io>
2021-12-08 21:34:46 -0500

    Add day 2

---
+++ day02/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import fileinput
+from functools import reduce
+from operator import mul
+
+
+def paper(l, w, h):
+    sides = (l * w, w * h, h * l)
+    return 2 * sum(sides) + min(sides)
+
+
+def part1(data):
+    return sum(paper(*entry) for entry in data)
+
+
+def part2(data):
+    return sum(reduce(mul, entry, 1) + 2 * sum(entry[:2]) for entry in data)
+
+
+def main():
+    data = [
+        tuple(sorted(map(int,
+                         line.rstrip().split('x'))))
+        for line in fileinput.input()
+    ]
+    print(part1(data))
+    print(part2(data))
+
+
+main()
