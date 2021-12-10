7719fc3f46bfec07ca7688c0ac9d4181b4457e08
Lukas Deutz <github@deutz.io>
2021-12-10 17:58:33 -0500

    Add template branch with all days

---
+++ day19/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import fileinput
+
+
+def solve(data):
+    pass
+
+
+def main():
+    data = [line.rstrip() for line in fileinput.input()]
+    print(solve(data))
+    print(solve(data))
+
+
+main()
