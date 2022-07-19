d8cb79f5fd60e434188518c7088bc64b64f57174
Lukas Deutz <gitlab@deutz.io>
2022-07-19 11:27:49 -0400

    Add day 4

--- day04/main.py
+++ day04/main.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 import fileinput
+import hashlib
 
 
-def solve(data):
-    pass
+def solve(data, zeroes):
+    idx = 0
+    target = "0" * zeroes
+    while (
+        not hashlib.md5(f"{data}{idx}".encode("utf-8")).hexdigest().startswith(target)
+    ):
+        idx += 1
+    return idx
 
 
 def main():
-    data = [line.rstrip() for line in fileinput.input()]
-    print(solve(data))
-    print(solve(data))
+    data = fileinput.input().readline().rstrip()
+    print(solve(data, 5))
+    print(solve(data, 6))
 
 
 main()
