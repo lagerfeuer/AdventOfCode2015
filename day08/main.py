0e504f7e96f135bc573aeb5f5760ac2505f271b9
Lukas Deutz <github@deutz.io>
2022-10-14 23:01:29 -0400

    Add day 8

--- day08/main.py
+++ day08/main.py
@@ -2,14 +2,20 @@
 import fileinput
 
 
-def solve(data):
-    pass
+def solve1(data):
+    length = lambda line: len(line[1:-1].encode("ascii").decode("unicode_escape"))
+    return sum(map(len, data)) - sum(map(length, data))
+
+
+def solve2(data):
+    length = lambda line: line.count("\\") + line.count('"') + len(line) + 2
+    return sum(map(length, data)) - sum(map(len, data))
 
 
 def main():
     data = [line.rstrip() for line in fileinput.input()]
-    print(solve(data))
-    print(solve(data))
+    print(solve1(data))
+    print(solve2(data))
 
 
 main()
