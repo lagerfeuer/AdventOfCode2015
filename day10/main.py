f235bfc84236e4cea1fea2dd480e8219d2acce72
Lukas Deutz <github@deutz.io>
2022-10-15 21:11:13 -0400

    Add day 10

--- day10/main.py
+++ day10/main.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 import fileinput
+from itertools import groupby
 
 
-def solve(data):
-    pass
+def encode(s):
+    encoded = [(char, len(list(group))) for char, group in groupby(list(s))]
+    return "".join(f"{l}{c}" for (c, l) in encoded)
+
+
+def solve(data, n):
+    for _ in range(n):
+        data = encode(data)
+    return data
 
 
 def main():
-    data = [line.rstrip() for line in fileinput.input()]
-    print(solve(data))
-    print(solve(data))
+    data = fileinput.input().readline().rstrip()
+    print(len(solve(data, 40)))
+    print(len(solve(data, 50)))
 
 
 main()
