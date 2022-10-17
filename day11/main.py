9659848e020cac30415cda558c9c199b38170a1c
Lukas Deutz <github@deutz.io>
2022-10-17 19:18:04 -0400

    Add day 11

--- day11/main.py
+++ day11/main.py
@@ -2,14 +2,55 @@
 import fileinput
 
 
+def valid(pw):
+    double = set()
+    incr = set()
+
+    if any(c in pw for c in "iol"):
+        return False
+
+    for (l, r) in zip(pw, pw[1:]):
+        if l == r:
+            double.add((l, r))
+    if len(double) < 2:
+        return False
+
+    for (c1, c2, c3) in zip(pw, pw[1:], pw[2:]):
+        if ord(c1) + 2 == ord(c2) + 1 == ord(c3):
+            incr.add((c1, c2, c3))
+    if len(incr) == 0:
+        return False
+
+    return True
+
+
+def pwgen(pw):
+    newpw = list(pw)
+    init = True
+    while not valid(newpw) or init:
+        init = False
+        idx = len(newpw) - 1
+        incr = True
+        while incr:
+            if newpw[idx] != "z":
+                newpw[idx] = chr(ord(newpw[idx]) + 1)
+                incr = False
+            else:
+                newpw[idx] = "a"
+                idx -= 1
+    return "".join(newpw)
+
+
 def solve(data):
-    pass
+    return pwgen(data)
 
 
 def main():
-    data = [line.rstrip() for line in fileinput.input()]
-    print(solve(data))
-    print(solve(data))
+    pw = fileinput.input().readline().rstrip()
+    pw = solve(pw)
+    print(pw)
+    pw = solve(pw)
+    print(pw)
 
 
 main()
