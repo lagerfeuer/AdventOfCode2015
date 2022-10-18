12790479ba84f9392ea277d1628f1d0247e470d8
Lukas Deutz <github@deutz.io>
2022-10-17 20:38:17 -0400

    Add day 12

--- day12/main.py
+++ day12/main.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
 import fileinput
+import json
 
 
-def solve(data):
-    pass
+def solve(obj, ignore=None):
+    def _solve(obj):
+        match obj:
+            case int():
+                return obj
+            case list():
+                return sum(map(_solve, obj))
+            case dict() if ignore not in obj.values():
+                return sum(map(_solve, obj.values()))
+            case _:
+                return 0
+
+    return _solve(obj)
 
 
 def main():
-    data = [line.rstrip() for line in fileinput.input()]
+    data = json.loads("".join(line for line in fileinput.input()))
     print(solve(data))
-    print(solve(data))
+    print(solve(data, ignore="red"))
 
 
 main()
